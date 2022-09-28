0b70eb131bda256ce7790a727d6c3d59fcae3099
Nurgül <113512193+nurgulcengel@users.noreply.github.com>
2022-09-28 21:28:24 +0300

    Update Program.cs

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -8,55 +8,55 @@ namespace Conditionals
 {
     class Program
     {
-        static void Main(string[] args)
+         static void Main(string[] args)
         {
-            //var number = 150;
-            //if (number >= 50&& number<=100)
-            //{
-            //    Console.WriteLine("Geçtiniz");
-            //}
-            //else if (number>=0 && number<50)
-            //{
-            //    //Console.WriteLine("Kaldınız");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Hatalı not girdiniz!");
-            //}
-            //Console.ReadLine();
-            //var bilbo = 5;
-            //Console.WriteLine(bilbo == 1 ? "salak" : "kalp");
-            //Console.ReadLine();
+            var number = 150;
+            if (number >= 50 && number <= 100)
+            {
+                Console.WriteLine("Geçtiniz");
+            }
+            else if (number >= 0 && number < 50)
+            {
+                //Console.WriteLine("Kaldınız");
+            }
+            else
+            {
+                Console.WriteLine("Hatalı not girdiniz!");
+            }
+            Console.ReadLine();
+            var bilbo = 5;
+            Console.WriteLine(bilbo == 1 ? "salak" : "kalp");
+            Console.ReadLine();
 
-            //char harfNotu = 'C';
-            //switch (harfNotu)
-            //{
-            //    case 'A':
-            //        Console.WriteLine("mük");
-            //        break;
-            //    case 'B':
-            //        Console.WriteLine("standart");
-            //        break;
-            //    case 'E':
-            //        Console.WriteLine("kotu");
-            //        break;
-            //    default :
-            //        Console.WriteLine("hatalı");
-            //        break;
-            //}
-            //Console.ReadLine();
+            char harfNotu = 'C';
+            switch (harfNotu)
+            {
+                case 'A':
+                    Console.WriteLine("mük");
+                    break;
+                case 'B':
+                    Console.WriteLine("standart");
+                    break;
+                case 'E':
+                    Console.WriteLine("kotu");
+                    break;
+                default:
+                    Console.WriteLine("hatalı");
+                    break;
+            }
+            Console.ReadLine();
 
-            //string medeniDurumu = "evli";
-            //bool cocukVarmı = false;
-            //if (medeniDurumu == "evli")
-            //{
-            //    Console.WriteLine(cocukVarmı != false ? "300 agi" : "200 agi");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("agi 100");
-            //}
-            //Console.ReadLine();    
+            string medeniDurumu = "evli";
+            bool cocukVarmı = false;
+            if (medeniDurumu == "evli")
+            {
+                Console.WriteLine(cocukVarmı != false ? "300 agi" : "200 agi");
+            }
+            else
+            {
+                Console.WriteLine("agi 100");
+            }
+            Console.ReadLine();
         }
         
 
